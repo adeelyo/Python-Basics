d820858c7a0de218aedf8797ae569e4d99c7cf20
adeelyo <adeel.masood1997@gmail.com>
2017-09-28 00:01:55 +0530

    decrement of a postitive number

---
+++ dec.py
@@ -0,0 +1,9 @@
+import sys
+a = int(input("Enter a positive number"))
+while a < 0 :
+   print("Enter a positive number please")
+   a = int(input())
+print("PRINTING")
+while a >= 0 :
+   print(" ",a)
+   a= a - 1
