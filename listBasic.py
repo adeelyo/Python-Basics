ff4c926ba7828edd431d844f98a49fe1dc0a4734
adeelyo <adeel.masood1997@gmail.com>
2017-10-20 16:07:28 +0530

    Basic usage of list dataType in python is explained

---
+++ listBasic.py
@@ -0,0 +1,7 @@
+fam = [1.1,2.2,3.3,4.4,5.5]
+fam1 = ["adeel",1.1,"Jule",2.2,"John",3.3,"Jet",4.4]
+fam2 = [["adeel",1.1],["Jule",2.2],["John",3.3],["jet",4.4]]
+print("fam: ",fam)
+print("fam1: ",fam1)
+print("fam2: ",fam2)
+print("The type of these variables is: ",type(fam2))
