5e43ddfe4dd2af4f355ff3d9e6b277e4f4dbe363
adeelyo <adeel.masood1997@gmail.com>
2017-10-20 16:05:54 +0530

    function like type are explained in detail

---
+++ type.py
@@ -0,0 +1,8 @@
+weight = 68.7
+height = 1.79
+bmi = weight / (height**2)
+x = "This is a program to calculate bmi and telling types of variables"
+print("Type of bmi is: ")
+print(type(bmi))
+print("Type of x is: ")
+print(type(x))
